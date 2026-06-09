51deb68b78bd362cb4935fc9df190d5618d0f1ce
Artem Romankov <a9636355354@gmail.com>
2026-06-09 22:22:45 +0300

    comment

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -12,6 +12,10 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
     
+    //Комментарий
+    /*/
+     * многострочный комментарий
+    /*/
     void Start()
     {
         Time.timeScale = _timeScale;
